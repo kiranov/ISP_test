5f84d131e8722e30e01cbb9c1c79f763564ab8a2
kiranov <kiranov.dm@phystech.edu>
2020-03-31 18:34:11 +0300

    fix codestyle in app.py, fix and moved readme, moved requirements.txt

--- service/app.py
+++ service/app.py
@@ -1,33 +1,42 @@
+'''application for determining whether one column in a text or several'''
 import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
 
-def stupid_compare(summ, pattern):
-    if all(summ >= pattern):
+
+def stupid_compare(sum_of_intensity, pattern):
+    '''compare intensity of the pattern and part of sum_of_intensity'''
+    if all(sum_of_intensity >= pattern):
         return True
     return False
+
 
 UPLOAD_FOLDER = './downloads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+APP = Flask(__name__)
+APP.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 def allowed_file(filename):
+    '''check that files are allowed'''
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+
+@APP.route('/', methods=['GET', 'POST'])
 def upload_file():
+    '''main page'''
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(APP.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
     return '''
@@ -40,57 +49,55 @@ def upload_file():
     </form>
     '''
 
-from flask import send_from_directory
 
-def detector(image, k = 0.97986979, size = int(10.17013889), length = int(29.88541667)):
+def trimming_edges(sum_of_intensity, max_intensity, k):
+    '''trimming edges of the document, if the document is too sparse
+        return -1'''
+    left_edge = 0
+    while sum_of_intensity[left_edge] >= (max_intensity * k):
+        left_edge += 1
+        if left_edge >= len(sum_of_intensity):
+            return -1
+    right_edge = 1
+    while (sum_of_intensity[len(sum_of_intensity) - right_edge] >=
+           (max_intensity * k)):
+        right_edge += 1
+    return left_edge, right_edge
+
+
+def detector(image, k=0.97986979, size=int(10.17013889),
+             length=int(29.88541667)):
+    '''detector of the columns'''
     gray = rgb2gray(image)
-    summ = gray.sum(axis = 0)
-    maxx = max(summ)
-    pattern = np.ones(size) * maxx * k
-    i = 0
-    #график
-    #plt.figure(figsize=(10, 8))
-    #fontsize=25
-    #plt.hlines(maxx * k, 0, gray.shape[1])
-    #plt.plot(summ, 'r', label = 'summ')
-    #plt.legend(fontsize=fontsize)
-    #
-    while summ[i] >= (maxx * k):
-        i += 1
-        if i >= len(summ):
-            return "Bad picture"
-    j = 1
-    while summ[len(summ) - j] >= (maxx * k):
-        j += 1
-    #print("i = ", i," j = ", j)
-    #if(i < j):
-    #    i = j
-    ind1 = i - 1
-    while i < len(summ)-size-1-j:
-        if summ[i] >= maxx * k:
-            ind2 = i
-            if ind2 - ind1 <= length:
-                ind1 = ind2
+    sum_of_intensity = gray.sum(axis=0)
+    max_intensity = max(sum_of_intensity)
+    pattern = np.ones(size) * max_intensity * k
+    left_edge, right_edge = trimming_edges(sum_of_intensity, max_intensity, k)
+    if(left_edge == -1):
+        return "too sparse document"
+    first_index = left_edge - 1
+    while left_edge < len(sum_of_intensity)-size-1-right_edge:
+        if sum_of_intensity[left_edge] >= max_intensity * k:
+            second_index = left_edge
+            if second_index - first_index <= length:
+                first_index = second_index
             else:
-                if stupid_compare(summ[i:i+size], pattern):
+                if stupid_compare(sum_of_intensity[left_edge:left_edge+size],
+                                  pattern):
                     return "Many Columns"
-        i += 1
+        left_edge += 1
     return "One Column"
 
 
-@app.route('/uploads/<filename>')
-#пропускаем столбцы из одного слова, не считаем их столбцами
-#используются параметры, полученный оптимизацией "функции ошибок"
-#%matplotlib inline
-#0.97986979, 10.17013889, 29.88541667
+@APP.route('/uploads/<filename>')
 def uploaded_file(filename):
-    #return plt.imread(UPLOAD_FOLDER + '/' + filename)
-    #if request.method == 'POST':
-    #    return redirect(url_for('upload_file'))
-
-    return detector(plt.imread(UPLOAD_FOLDER + '/' + filename)) + '''<div><button style="margin=20px;" onclick="window.location='/'">Back</button></div>'''
+    '''page with the results of the algorithm'''
+    result = detector(plt.imread(UPLOAD_FOLDER + '/' + filename))
+    result += '''<div><button style="margin=20px;"
+    onclick="window.location='/'">Back</button></div>'''
+    return result
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0')
+    APP.debug = True
+    APP.run(host='0.0.0.0')
